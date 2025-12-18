5afad7e026ef48a386d86d8584308f38ae6429c2
Daniel <dan1902heller@gmail.com>
2025-12-18 18:56:59 -0300

    feat: add SQLite setup with users table creation

--- src/JavaCrud.java
+++ src/JavaCrud.java
@@ -3,29 +3,46 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JavaCrud {
-    public static void connect(){
+    protected static void connect() {
 
         String url = "jdbc:sqlite:data.db";
         File dbFile = new File("data.db");
         boolean existed = dbFile.exists();
 
-        try (var conn = DriverManager.getConnection(url)){
-            System.out.println("Connection to SQlite established"); // Debug message
+
+        var sql = "CREATE TABLE IF NOT EXISTS users ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "nome TEXT NOT NULL, "
+                + "email TEXT NOT NULL UNIQUE, "
+                + "phone_num TEXT"
+                + ");";
+
+
+        try (var conn = DriverManager.getConnection(url);
+            var stmt = conn.createStatement()) {
+
+            stmt.execute(sql);
+
+            System.out.println("Connection to SQlite established");
             if (existed) {
                 System.out.println("Database already existed.");
             } else {
                 System.out.println("Database created.");
             }
 
-        } catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    protected static void insertData(){
+
+        String url = "jdbc:sqlite:data.db";
+
+    }
+
     public static void main(String[] args) {
 
-        //connect();
-
-
+        connect();
     }
 }
